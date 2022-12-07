def525cf023d1a91ad7b8b12e956405866097a12
Elcan <eljanng@code.edu.az>
2022-12-08 02:05:53 +0400

    First C#

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,103 @@
+﻿
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//	Console.WriteLine("bolunur");
+//}
+//else
+//{
+//	Console.WriteLine("bolunmur");
+//}
+
+
+
+
+
+//int n = 6;
+
+//int m = 10;
+
+//int sum = 0;
+
+//if (n % 2 == 0 && m % 2 == 0) 
+//{
+//	Console.WriteLine(n + m);
+//}
+//else
+//{
+//	Console.WriteLine("duzgun reqem daxil edin");
+//}
+
+
+
+
+
+
+//int n = 1;
+
+//int m = 10;
+
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//	if (i % 2 == 1)
+//		sum += i;
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+//int[] array = { 2, 5, 7, 9, 11 };
+//int sum = 0;
+
+//foreach (int item in array)
+//{
+//    if (item % 2 == 1) 
+//    sum += item;
+//}
+//Console.WriteLine(sum);
+
+
+
+//int[] array = { 2, 4, 7, 9, 10 };
+//int count = 0;
+//foreach (int item in array) 
+//{
+//    if (item % 2 == 0) 
+//        count++;
+//}
+//Console.WriteLine(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
